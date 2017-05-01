8a83887c002e2017c80cc93732de572cd5757d89
Olga <oljaaste@gmail.com>
2017-05-01 21:23:27 +0300

    fivth commit

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,34 @@ $(document).ready(function(){
 
 
 
+var slideWidth=300;
+var sliderTimer;
+$(function(){
+$('.slider').width($('.slider').children().size()*slideWidth);
+    sliderTimer=setInterval(nextSlide,4000);
+    $('.slide-section__info').hover(function(){
+        clearInterval(sliderTimer);
+    },function(){
+        sliderTimer=setInterval(nextSlide,4000);
+    });
+});
+
+function nextSlide(){
+    var currentSlide=parseInt($('.slider').data('current'));
+    currentSlide++;
+    if(currentSlide>=$('.slider').children().size())
+    {
+        currentSlide=0;   
+    }
+    $('.slider').animate({left: -currentSlide*slideWidth},300).data('current',currentSlide);
+}
+
+
+
+
+
+
+
   $('.team__link').click(function(e) { 
       e.preventDefault();
       $(this).closest('.team').find('.team__manager').show();
