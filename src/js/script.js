b6df162db5b785f0e9689257c131c83f96a78168
Olga <oljaaste@gmail.com>
2017-05-06 21:42:31 +0300

    eleventh commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   $('.nav-burger').click(function(){
     $(this).toggleClass('nav-burger--hamburger nav-burger--close');
     $('.main-nav').toggleClass('main-nav--open');
@@ -6,26 +7,46 @@ $(document).ready(function(){
 
 
 
-$('.slider').slick({
-  mobileFirst: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  centerMode: true,
-  centerPadding: '0',
-  arrows: false,
-  dots: true,
-  infinite: true,
-  // speed: 300,
-  // autoplay: true,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: true,
+  $('#slider-id').slick({
+    mobileFirst: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '0',
+    arrows: false,
+    dots: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+        }
       }
-    }
-  ]
-});
+    ]
+  });
+
+
+
+  $('#reviews-id').slick({
+    mobileFirst: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '0',
+    arrows: false,
+    dots: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+        }
+      }
+    ]
+  });
+
 });
 
 
